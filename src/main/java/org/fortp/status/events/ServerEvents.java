37100f3af1e8ad66a41d881a6b6f933300bdf923
Fort-P <fortroberts2006@gmail.com>
2025-09-13 16:51:37 -0500

    Send join message

--- src/main/java/org/fortp/status/events/ServerEvents.java
+++ src/main/java/org/fortp/status/events/ServerEvents.java
@@ -1,10 +1,14 @@
 package org.fortp.status.events;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Colors;
 import net.minecraft.util.Formatting;
 import org.fortp.status.Status;
 import org.fortp.status.utils.PlayerData;
@@ -13,7 +17,28 @@ public class ServerEvents {
     private static int lastTime = -1; // -1 means "not initialized yet"
 
     public static void init() {
-            ServerTickEvents.END_SERVER_TICK.register(ServerEvents::onTick);
+        ServerTickEvents.END_SERVER_TICK.register(ServerEvents::onTick);
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            handler.getPlayer().sendMessage(Text.literal("[Status]")
+                            .setStyle(Style.EMPTY.withColor(Colors.GREEN)
+                            .withClickEvent(new ClickEvent.RunCommand("/status"))
+                            .withHoverEvent(new HoverEvent.ShowText(Text.literal("Click to set your status!"))))
+                    .append(Text.literal(" Don't forget to set your status!")
+                            .setStyle(Style.EMPTY.withColor(Colors.WHITE)
+                            .withClickEvent(new ClickEvent.RunCommand("/status"))
+                            .withHoverEvent(new HoverEvent.ShowText(Text.literal("Click to set your status!"))))));
+        });
+
+        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+            if (Status.noSleepers.contains(handler.getPlayer().getUuid())) {
+                Status.noSleepers.remove(handler.getPlayer().getUuid());
+                if (Status.noSleepers.isEmpty()) {
+                    server.getPlayerManager().broadcast(Text.literal("All players are now good to sleep!")
+                            .setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA)), false);
+                }
+            }
+        });
     }
 
     private static void onTick(MinecraftServer server) {
